0b2d6d3a48eecc88179cbe39855cf78e23aa686c
kp1153 <kamta_tiwari@yahoo.com>
2025-12-12 17:31:45 +0530

    force deploy

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./sanity/schemaTypes";
 
@@ -9,7 +9,7 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   basePath: "/studio",
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
